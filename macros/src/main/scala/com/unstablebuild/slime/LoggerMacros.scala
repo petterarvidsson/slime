eea4901c656fa0d027ec9c9c238e6361b194230b
Lucas Torri <lucastorri@gmail.com>
2017-07-20 12:18:01 +0200

    remove trait extends

--- macros/src/main/scala/com/unstablebuild/slime/LoggerMacros.scala
+++ macros/src/main/scala/com/unstablebuild/slime/LoggerMacros.scala
@@ -27,7 +27,7 @@ object LoggerMacros {
             ..$loggingMethods
           }
         """
-      case Some(q"abstract trait $name extends ..$parents") =>
+      case Some(q"abstract trait $name") =>
 
         val companion = annottees.map(_.tree).collectFirst {
           case obj @ q"object $_ extends $_ { ..$_ }" =>
@@ -38,7 +38,7 @@ object LoggerMacros {
         val loggingSignatures = for (level <- levels; size <- params) yield generator.def_log_signature(level, size)
 
         val code = s"""
-          trait $name extends ..$parents" {
+          trait $name {
             ${baseSignatures.mkString("", ";\n", ";\n")}
             ${loggingSignatures.mkString("", ";\n", ";\n")}
           }
@@ -50,6 +50,35 @@ object LoggerMacros {
     }
 
     c.Expr[Any](result.asInstanceOf[c.Tree])
+  }
+
+  def interface(c: blackbox.Context): c.Expr[Array[String]] = {
+    val generator = new LoggerGenerator(c)
+    import c.universe._
+
+    val baseSignatures = for (level <- levels) yield generator.def_isEnabled_signature(level)
+    val loggingSignatures = for (level <- levels; size <- params) yield generator.def_log_signature(level, size)
+
+    val source =
+      s"""
+        package com.unstablebuild.slime {
+          trait Logger {
+            ${baseSignatures.mkString("", ";\n", ";\n")}
+            ${loggingSignatures.mkString("", ";\n", ";\n")}
+          }
+          object Logger {
+            def apply[T]()(implicit ct: scala.reflect.ClassTag[T]): Logger = apply(ct.runtimeClass)
+            def apply[T](clazz: Class[T]): Logger = apply(clazz.getName)
+            def apply(name: String): Logger = new com.unstablebuild.slime.gen.MacroLogger(name)
+          }
+        }
+       """
+
+    // They are split into lines to avoid problems with maximum string sizes
+    // https://stackoverflow.com/questions/17382067/scala-long-strings-error
+    val prettyPrintedLines = c.universe.showCode(c.parse(source)).split("\n")
+
+    c.Expr(q"$prettyPrintedLines")
   }
 
   case class Level(name: String) {
