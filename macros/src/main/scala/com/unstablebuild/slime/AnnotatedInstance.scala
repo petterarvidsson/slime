9908a0c3d93a1c409e33fd1b3ab653a26219c079
Lucas Torri <lucastorri@gmail.com>
2017-07-29 21:26:56 +0200

    add tests for the encoder

--- macros/src/main/scala/com/unstablebuild/slime/AnnotatedInstance.scala
+++ macros/src/main/scala/com/unstablebuild/slime/AnnotatedInstance.scala
@@ -5,3 +5,10 @@ case class AnnotatedInstance[T](instance: T, encoder: TypeEncoder[T]) {
   def encoded: Seq[(String, Value)] = encoder.encode(instance)
 
 }
+
+object AnnotatedInstance {
+
+  def from[T](instance: T)(implicit encoder: TypeEncoder[T]): AnnotatedInstance[T] =
+    apply(instance, encoder)
+
+}
---
+++ src/test/scala/com/unstablebuild/slime/EncoderTest.scala
@@ -0,0 +1,90 @@
+package com.unstablebuild.slime
+
+import ch.qos.logback.classic.spi.LoggingEvent
+import org.scalatest.{FlatSpec, MustMatchers}
+import ch.qos.logback.classic.Level
+import org.slf4j.{MDC, Marker}
+
+import scala.collection.mutable
+
+class EncoderTest extends FlatSpec with MustMatchers { self =>
+
+  it must "return the output of the format" in new encoderContext {
+    encoder.encode(event()) must equal(format.format(Seq.empty))
+  }
+
+  it must "encode annotated values" in new encoderContext with TypeEncoders {
+
+    val annotations = Seq(AnnotatedInstance.from("one" -> 1), AnnotatedInstance.from("two" -> 2))
+    encoder.setFields("level")
+    encoder.encode(event(marker = new AnnotationMarker(annotations)))
+
+    format.receivedValues must equal(
+      Seq("level" -> StringValue("TRACE"), "one" -> NumberValue(1), "two" -> NumberValue(2))
+    )
+  }
+
+  it must "add level and message" in new encoderContext {
+
+    encoder.setFields("level,message")
+    encoder.encode(event(msg = "hi!", level = Level.WARN))
+
+    format.receivedValues must equal(Seq("level" -> StringValue("WARN"), "message" -> StringValue("hi!")))
+  }
+
+  it must "add mdc" in new encoderContext {
+
+    encoder.setFields("mdc")
+    MDC.put("hello", "world")
+    encoder.encode(event())
+
+    format.receivedValues must equal(Seq("mdc" -> NestedValue(Seq("hello" -> StringValue("world")))))
+  }
+
+  it must "add timestamp" in new encoderContext {
+    encoder.setFields("timestamp")
+    encoder.encode(event(ts = 12345L))
+
+    format.receivedValues must equal(Seq("timestamp" -> NumberValue(12345L)))
+  }
+
+  it must "add thread and logger" in new encoderContext {
+    encoder.setFields("thread, logger")
+    encoder.encode(event())
+
+    format.receivedValues must equal(
+      Seq("thread" -> StringValue(Thread.currentThread().getName), "logger" -> StringValue(self.getClass.getName))
+    )
+  }
+
+  trait encoderContext {
+
+    val encoder = new Encoder
+    val format = new RecodingFormat
+    encoder.setFormat(format)
+
+  }
+
+  def event(msg: String = "", level: Level = Level.TRACE, ts: Long = 0, marker: Marker = null): LoggingEvent = {
+    val event = new LoggingEvent()
+    event.setLevel(level)
+    event.setMessage(msg)
+    event.setTimeStamp(ts)
+    event.setThreadName(Thread.currentThread().getName)
+    event.setLoggerName(getClass.getName)
+    event.setMarker(marker)
+    event
+  }
+
+  class RecodingFormat extends Format {
+    private val allValues = mutable.ListBuffer.empty[Seq[(String, Value)]]
+
+    override def format(values: Seq[(String, Value)]): Array[Byte] = {
+      allValues += values
+      "hi".getBytes()
+    }
+
+    def receivedValues: Seq[(String, Value)] = allValues.flatten
+  }
+
+}
